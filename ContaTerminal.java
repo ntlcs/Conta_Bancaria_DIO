2fb2e7f26543870ebc98a78390085f689b15b000
Natalia - ntlcs <nataliadacostasilva@gmail.com>
2024-05-13 14:13:30 -0300

    minha primeira versao

---
+++ ContaTerminal.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class ContaTerminal {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Por favor, digite o número da Agência: ");
+        String numeroAgencia = scanner.nextLine();
+
+        System.out.print("Digite o nome do cliente: ");
+        String nomeCliente = scanner.nextLine();
+
+        System.out.print("Digite o saldo da conta: ");
+        double saldo = scanner.nextDouble();
+
+        System.out.println("Olá " + nomeCliente + ", obrigado por criar uma conta em nosso banco.");
+        System.out.println("Sua agência é " + numeroAgencia + ", conta " + nomeCliente + " e seu saldo é R$" + saldo);
+
+        scanner.close();
+    }
+}
---
+++ Readme.md
@@ -0,0 +1,13 @@
+# Conta_Bancaria_DIO
+
+Olá! Eu sou o **CoderNCS** e criei este projeto chamado **Conta_Bancaria_DIO** para simular uma conta bancária. Neste programa, você pode inserir os dados de uma conta, como número da agência, nome do cliente e saldo, e ele exibirá uma mensagem personalizada com essas informações.
+
+É um exercício parte do **BOOTCAMP Santander JAVA Back-End**
+
+## Como usar
+
+1. Clone este repositório para a sua máquina local.
+2. Abra o projeto em sua IDE favorita (como Eclipse, IntelliJ ou NetBeans).
+3. Execute a classe `ContaTerminal.java`.
+4. Siga as instruções no terminal para inserir os dados da conta.
+5. O programa exibirá uma mensagem com os detalhes da conta.
